68157aba63825add7ba33f17f1da498657a109e8
Кирилл Артименя <artsimenia@newit.by>
2023-12-20 18:43:17 +0300

    update

--- src/filling/skills.js
+++ src/filling/skills.js
@@ -1,5 +1,13 @@
 export default [
   {
+    name: 'Laravel',
+    info: 'Весь основной функционал (controllers, models, middleware и т.д)',
+  },
+  {
+    name: 'PHP',
+    info: 'ООП, ссылки, Type-hinting, copy-on-write и так далее. ',
+  },
+  {
     name: 'HTML/CSS',
     info: 'Изучил семантические теги HTML5 и умею их применять. Умею  работать с анимациями. Использую в верстке Flexbox и Grid, а также методологию BEM. Работаю с препроцессором SASS(SCSS).',
   },
@@ -12,15 +20,7 @@ export default [
     info: 'На хорошем уровне знаю React | Hooks. Работаю с Redux, Redux-Toolkit (Thunks). Все в связке с TypeScript. Знаком с основными UI библиотеками (Ant Design, React Bootstrap). Также работаю с react-router-dom, axios',
   },
   {
-    name: 'PHP',
-    info: 'ООП, ссылки, Type-hinting, copy-on-write и так далее. ',
-  },
-  {
     name: 'Прочие навыки',
     info: 'Умею работать с системой контроля версий git. Для верстки использую сборщик Gulp.',
-  },
-  {
-    name: 'Laravel',
-    info: 'Весь основной функционал (controllers, models, middleware и т.д)',
   },
 ];
